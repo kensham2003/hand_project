35a5f0799fc8ce2cbf152647b21f95ff78977afa
HAL_Taiga <kyomachi38@gmail.com>
2023-10-04 11:34:16 +0900

    カードクラス、モンスターカード、呪文カード追加

---
+++ Assets/Script/MonsterCard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MonsterCard : Card
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        image.color = Color.red;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+     //効果発動
+    public override void CardEffect(RaycastHit hit)
+    {
+        Instantiate(SpawnObject, hit.point, Quaternion.identity);
+    }
+}
