66b555cdc89973c4dbe944d76b1fe6046fc80782
HAL_Taiga <kyomachi38@gmail.com>
2023-10-11 15:48:54 +0900

    コピペ修正 オールコピペ実装

--- Assets/Script/InstantiateManager.cs
+++ Assets/Script/InstantiateManager.cs
@@ -25,11 +25,14 @@ public class InstantiateManager : MonoBehaviour
     public void InstantiateMonster(int cardId, Vector3 position, Quaternion rotation){
         GameObject monsterPrefab = CardMonsterDictionary.Instance.GetMonsterPrefab(cardId);
         GameObject monsterObj = PoolManager.Instance.GetGameObject(monsterPrefab, position, rotation);
+        
         Monster m = monsterObj.GetComponent<Monster>();
         m.visibleList = visibleList;
         m.cpuMain = cpuMain;
         m.instantiateManager = this;
         m.paramerter = CardMonsterDictionary.Instance.GetMonsterParamerter(cardId);
+        //追加　ID設定
+        m.paramerter.monsterID = cardId;
         cpuMain.UsageRegister(m.paramerter.spawnLoad);
         //Debug.Log("生成 : " + m.paramerter.spawnLoad.raiseRate);
     }
---
+++ Assets/Script/Monster/AllCopyPaste.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllCopyPaste : EffectMonster
+{
+    //半径
+    [Tooltip("半径")]
+    [SerializeField] float spawnRange = 2;
+    public List<GameObject> t;
+    //シーン上にあるすべてのPlayerMonsterID
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン上のPlayerMonster収集&生成
+        foreach(GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            if(obj.tag == "Player")
+            {
+                Spawn(obj,obj.GetComponent<PlayerMonster>().paramerter.monsterID);
+            }
+        }
+
+        //Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(GameObject obj,int id)
+    {
+        Vector3 direction;
+        Vector3 spawnPosition = obj.transform.position;
+
+        direction.x = Random.Range(-1.0f,1.0f);
+        direction.z = Random.Range(-1.0f,1.0f);
+        direction.y = 0;
+
+        spawnPosition += direction * spawnRange;
+
+        //スポーン
+        GameObject.Find("Managers").GetComponent<InstantiateManager>().
+        InstantiateMonster(id, spawnPosition, Quaternion.identity);
+    }
+
+
+}
