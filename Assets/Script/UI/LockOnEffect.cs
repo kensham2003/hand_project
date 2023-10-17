24384e3a33cf0dda9635bb53ae11ca4e2f6ddef5
HAL_Taiga <kyomachi38@gmail.com>
2023-10-17 18:43:04 +0900

    ビルド時にデバッグログを表示

--- Assets/Script/UI/LockOnEffect.cs
+++ Assets/Script/UI/LockOnEffect.cs
@@ -8,10 +8,15 @@ public class LockOnEffect : MonoBehaviour
     /// 表示するオブジェクト（位置）
     /// </summary>
     private GameObject m_target = null;
+
+    /// <summary>
+    /// 自動で消える時間
+    /// </summary>
+    [SerializeField] private float m_lifeTime = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("Death",m_lifeTime);
     }
 
     // Update is called once per frame
@@ -21,10 +26,22 @@ public class LockOnEffect : MonoBehaviour
         {
             transform.position = m_target.transform.position;
         }
+        else
+        {
+            //ターゲットが死んだら自滅
+            Destroy(this.gameObject);
+        }
     }
 
     public void SetTarget(GameObject obj)
     {
         m_target = obj;
     }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+
 }
